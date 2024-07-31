994f3070a777329cc254678fc0a3b7378b52d85d
Jboy422 <gitonly543@gmail.com>
2024-07-31 12:49:30 +0900

    Initial Commit

---
+++ RemoteConfigTutorial/RemoteConfigTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigTutorialApp.swift
+//  RemoteConfigTutorial
+//
+//  Created by Jboy422 on 2024/07/31.
+//
+
+import SwiftUI
+
+@main
+struct RemoteConfigTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
